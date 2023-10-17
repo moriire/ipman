790a2c9c1d3fea23d9a5d3014b3d30f02f240ae1
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2023-10-17 10:55:24 +0000

    latest commit

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-  name = 'IPToolz',  
-  packages = ['IPToolz'],
+  name = 'ipman',  
+  packages = ['ipman'],
   package_dir={'ipman': 'ipman'},
   package_data={'ipman': ['mo.py','errors.py']},
               version = '1.1.1.1',
   license='MIT',    
-  description = 'IPToolz is a high-level core python package for Internet Protocol(IP) manipulations.',
+  description = 'ipman is a high-level core python package for Internet Protocol(IP) manipulations.',
   long_description=long_description,
   long_description_content_type="text/markdown",
   author = 'Mobolaji Abdulsalam',                   
   author_email = 'ibraheemabdulsalam@gmail.com',
-  url = 'https://github.com/moriire/IPToolz',  
-  download_url = 'https://github.com/moriire/IPToolz/archive/v_110.tar.gz',
-  keywords = ['IPToolz is a high-level core python package for Internet Protocol(IP) manipulations.'],
+  url = 'https://github.com/moriire/ipman',  
+  download_url = 'https://github.com/moriire/ipman/archive/v_110.tar.gz',
+  keywords = ['IPMan is a high-level core python package for Internet Protocol(IP) manipulations.'],
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
